00fdebe5f69124da89674a7c2c2d26a0a7db8ca8
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2021-02-05 12:33:52 -0500

    Arregla eslint

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
-const chalk = require('chalk');
+const chalk = require("chalk");
 
-const {config} = require('./config/config')
+const { config } = require("./config/config");
 
-app.get('/', (req, res, next) => {
-  res.send('Hello Word')
-})
+app.get("/", (req, res, next) => {
+  res.send("Hello Word");
+});
 
 app.listen(config.port, () => {
   console.log(
-    chalk.hex('#fff').bgBlue.bold(`litening http://localhost:${config.port}`),
+    chalk.hex("#fff").bgBlue.bold(`litening http://localhost:${config.port}`)
   );
 });
