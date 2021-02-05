ea22e6abb75c73dfcefbc3652b9fad738e33830b
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2021-02-05 12:06:19 -0500

    Sube archivos

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const chalk = require('chalk');
+
+const {config} = require('./config/config')
+
+app.get('/', (req, res, next) => {
+  res.send('Hello Word')
+})
+
+app.listen(config.port, () => {
+  console.log(
+    chalk.hex('#fff').bgBlue.bold(`litening http://localhost:${config.port}`),
+  );
+});
