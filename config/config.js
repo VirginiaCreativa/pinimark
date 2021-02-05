f0d2b26cbafc15040453bf773669e8e14b65126f
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2021-02-05 14:48:01 -0500

    Mantener Moongose

--- config/config.js
+++ config/config.js
@@ -1,8 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const config = {
-  dev: process.env.NODE_ENV !== 'production',
+  dev: process.env.NODE_ENV !== "production",
   port: process.env.PORT || 3000,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbNameUsers: process.env.DB_NAME_USERS,
 };
 
 module.exports = { config };
--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 const chalk = require("chalk");
 
 const { config } = require("./config/config");
+const MongooseLib = require("./lib/mongoose");
+
+// ====== CONNECT MONGODB ====== //
+const connect = new MongooseLib();
+connect.connect();
 
 app.get("/", (req, res, next) => {
   res.send("Hello Word");
