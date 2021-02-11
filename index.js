6616ba722acf1d4e1f58f529458d4c49cea4dbc0
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2021-02-11 07:40:05 -0500

    solucion fin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { config } = require("./config/config");
 const MongooseLib = require("./lib/mongoose");
 const UserRouter = require("./routes/User.Router");
 
-app.use(express.static(path.join(__dirname, "client")));
+app.use(express.static("client"));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "public", "index.html"));
 });
