1b7f155eafed4169a813d73ba860e9af4eb7c55d
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2021-02-15 09:21:53 -0500

    Implementa Header part 6 and stylelint

--- client/src/common/Logo/LogoIcon.jsx
+++ client/src/common/Logo/LogoIcon.jsx
@@ -4,9 +4,15 @@ import Logo from '../../assets/logo/logo_icon.svg';
 
 const LogoIcon = () => {
   return (
-    <Link to="/">
-      <img src={Logo} alt="Icono de Logo Pinimark" style={{ height: '50px' }} />
-    </Link>
+    <>
+      <Link to="/">
+        <img
+          src={Logo}
+          alt="Icono de Logo Pinimark"
+          style={{ height: '40px' }}
+        />
+      </Link>
+    </>
   );
 };
 
--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -2,24 +2,33 @@ import React from 'react';
 import styled from 'styled-components';
 import Variables from '../../styles/VariableStyled';
 
+const SearchStyled = styled.div`
+  position: relative;
+  box-icon[name='search'] {
+    position: absolute;
+    top: 4px;
+    right: 10px;
+  }
+`;
 const InputStyled = styled.input`
   border: none;
   border-radius: 100px;
   background-color: ${Variables.grey0};
   padding: 8px 16px;
-  width: 30%;
+  width: 20vw;
   font-size: 0.8rem;
 `;
 
 const Search = () => {
   return (
-    <>
+    <SearchStyled>
+      <box-icon name="search"></box-icon>
       <InputStyled
         type="text"
         name="search"
         placeholder="Buscador una palabra"
       />
-    </>
+    </SearchStyled>
   );
 };
 
